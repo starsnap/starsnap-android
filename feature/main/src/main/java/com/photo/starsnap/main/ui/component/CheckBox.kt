fe91ceac454d95c79f31947e75e54815687b5b88
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-17 23:45:19 +0900

    :bug: :: fix bug

--- feature/main/src/main/java/com/photo/starsnap/main/ui/component/CheckBox.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/component/CheckBox.kt
@@ -5,37 +5,53 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.photo.starsnap.designsystem.R
 
 @Composable
 fun CustomCheckBox(
-    checked: Boolean,
     onPainter: Painter,
     offPainter: Painter,
     onCheckedChange: (Boolean) -> Unit,
 ) {
+    var checkState by remember { mutableStateOf(false) }
+
     Box(
         modifier = Modifier.clickable(onClick = {
-            onCheckedChange(!checked)
+            checkState = !checkState
+            onCheckedChange(checkState)
         }, interactionSource = remember {
             MutableInteractionSource()
         }, indication = null
         )
 
     ) {
-        if (checked) {
+        if (checkState) {
             Icon(
                 painter = onPainter,
                 contentDescription = "on",
             )
-        }
-        if (!checked) {
+        } else {
             Icon(
                 painter = offPainter,
                 contentDescription = "off",
             )
         }
     }
+}
+
+
+@Composable
+fun PasswordEyeCheckBox(checkState: (Boolean) -> Unit) {
+    CustomCheckBox(
+        onPainter = painterResource(R.drawable.eye_open),
+        offPainter = painterResource(R.drawable.eye_close),
+        onCheckedChange = checkState
+    )
 }
