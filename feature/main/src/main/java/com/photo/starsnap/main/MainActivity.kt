7db9c0d87c9717d4e981b8e4055f0fba0d06d604
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-17 23:08:12 +0900

    :zap: :: remove Text import

--- feature/main/src/main/java/com/photo/starsnap/main/MainActivity.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.photo.starsnap.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
