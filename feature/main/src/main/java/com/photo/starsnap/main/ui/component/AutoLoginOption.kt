31663ce7fa9aa6ce73831ddb6a6086d363883165
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-21 02:05:10 +0900

    :lipstick: :: add AutoLoginOption

---
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/component/AutoLoginOption.kt
@@ -0,0 +1,50 @@
+package com.photo.starsnap.main.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.photo.starsnap.designsystem.R
+
+@Composable
+fun AutoLoginOption(checkState: (Boolean) -> Unit) {
+    var isChecked by remember { mutableStateOf(false) } // 상태 관리
+
+    Row(
+        modifier = Modifier.clickable(
+            onClick = {
+                isChecked = !isChecked // 상태 토글
+                checkState(isChecked) // 상태 전달
+            },
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null
+        ).padding(start = 5.dp, top = 10.dp, bottom = 6.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box {
+            Image(
+                painter = if (isChecked) painterResource(R.drawable.check_box_true) else painterResource(
+                    R.drawable.check_box_false
+                ),
+                contentDescription = if (isChecked) "Checked" else "Unchecked"
+            )
+        }
+        Text(
+            text = stringResource(R.string.auto_login),
+            modifier = Modifier.padding(start = 8.dp) // 텍스트와 체크박스 간 간격
+        )
+    }
+}
