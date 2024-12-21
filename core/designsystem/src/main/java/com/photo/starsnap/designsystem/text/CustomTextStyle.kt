73259b1994282807382f864c24e597877cf3a659
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-21 22:26:15 +0900

    :zap: :: add CustomTextStyle object

---
+++ core/designsystem/src/main/java/com/photo/starsnap/designsystem/text/CustomTextStyle.kt
@@ -0,0 +1,11 @@
+package com.photo.starsnap.designsystem.text
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.photo.starsnap.designsystem.text.TextFont.pretendard
+import com.photo.starsnap.designsystem.CustomColor
+
+object CustomTextStyle {
+
+}
