a0db2a4742184a25676842ba2edd80822570055e
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-21 22:26:24 +0900

    :zap: :: add TextFont object

---
+++ core/designsystem/src/main/java/com/photo/starsnap/designsystem/text/TextFont.kt
@@ -0,0 +1,19 @@
+package com.photo.starsnap.designsystem.text
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.photo.starsnap.designsystem.R
+
+object TextFont {
+    val pretendard = FontFamily(
+        Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.pretendard_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+        Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+        Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+        Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+        Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.pretendard_thin, FontWeight.Thin, FontStyle.Normal),
+    )
+}
