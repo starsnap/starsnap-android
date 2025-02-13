d5c017d4807d3292ac0f37ef821151a99fde5538
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-02-13 23:11:05 +0900

    :zap: :: add a membership status message (username, password, email)

---
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/component/Text.kt
@@ -0,0 +1,157 @@
+package com.photo.starsnap.main.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.photo.starsnap.designsystem.text.CustomTextStyle.hint1
+import com.photo.starsnap.designsystem.text.CustomTextStyle.hint2
+import com.photo.starsnap.designsystem.CustomColor.error
+import com.photo.starsnap.designsystem.CustomColor.success
+import com.photo.starsnap.designsystem.CustomColor.gray
+import com.photo.starsnap.designsystem.R
+import com.photo.starsnap.main.viewmodel.auth.PasswordState
+import com.photo.starsnap.main.viewmodel.auth.SignupViewModel
+import com.photo.starsnap.main.viewmodel.auth.State
+import com.photo.starsnap.main.viewmodel.auth.ValidState
+
+@Composable
+fun TextEditHint(text: String) {
+    Text(text, style = hint1)
+}
+
+// 닉네임 유효성 검사
+@Composable
+fun CheckUserNameStatusMessage(viewModel: SignupViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    val message = when (uiState.usernameValidState) {
+        ValidState.ERROR -> MessageState(
+            stringResource(R.string.signup_username_screen_error_message),
+            error
+        )
+
+        ValidState.EXIST -> MessageState(
+            stringResource(R.string.signup_username_screen_exist_message),
+            error
+        )
+
+        ValidState.SUCCESS -> MessageState(
+            stringResource(R.string.signup_username_screen_success_message),
+            success
+        )
+
+        ValidState.LOADING -> MessageState(
+            stringResource(R.string.signup_username_screen_loading_message),
+            gray
+        )
+
+        ValidState.DEFAULT -> MessageState(
+            stringResource(R.string.signup_username_screen_default_message),
+            gray
+        )
+    }
+
+    SignupStateMessage(message.text, message.color)
+}
+
+// 비밀번호 유효성 검사
+@Composable
+fun CheckPasswordStatusMessage(viewModel: SignupViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    val message = when (uiState.passwordValidState) {
+        PasswordState.ERROR -> MessageState(
+            stringResource(R.string.signup_password_screen_error_message),
+            error
+        )
+
+        PasswordState.SUCCESS -> MessageState(
+            stringResource(R.string.signup_password_screen_success_message),
+            success
+        )
+
+        PasswordState.DEFAULT -> MessageState(
+            stringResource(R.string.signup_password_screen_default_message),
+            gray
+        )
+
+        PasswordState.INVALID_CONFIRM -> MessageState(
+            stringResource(R.string.signup_password_screen_mismatch_message),
+            error
+        )
+    }
+
+    SignupStateMessage(message.text, message.color)
+}
+
+// 이메일 유효성 검사
+@Composable
+fun CheckEmailStatusMessage(viewModel: SignupViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    val message = when (uiState.emailValidState) {
+        ValidState.ERROR -> MessageState(
+            stringResource(R.string.signup_email_screen_error_message),
+            error
+        )
+
+        ValidState.EXIST -> MessageState(
+            stringResource(R.string.signup_email_screen_exist_message),
+            error
+        )
+
+        ValidState.SUCCESS -> MessageState(
+            stringResource(R.string.signup_email_screen_success_message),
+            success
+        )
+
+        ValidState.LOADING -> MessageState(
+            stringResource(R.string.signup_email_screen_loading_message),
+            gray
+        )
+
+        ValidState.DEFAULT -> MessageState(
+            stringResource(R.string.signup_email_screen_default_message),
+            gray
+        )
+    }
+
+    SignupStateMessage(message.text, message.color)
+}
+
+// 인증코드 유효성 검사
+@Composable
+fun CheckVerifyCodeStatusMessage(viewModel: SignupViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    val message = when (uiState.verifyState) {
+        State.ERROR -> MessageState(
+            stringResource(R.string.signup_verify_screen_error_message),
+            error
+        )
+
+        State.SUCCESS -> MessageState(
+            stringResource(R.string.signup_verify_screen_success_message),
+            success
+        )
+
+        State.LOADING -> MessageState(
+            stringResource(R.string.signup_verify_screen_loading_message),
+            gray
+        )
+
+        State.DEFAULT -> MessageState(
+            stringResource(R.string.signup_verify_screen_default_message),
+            gray
+        )
+    }
+
+    SignupStateMessage(message.text, message.color)
+}
+
+@Composable
+fun SignupStateMessage(text: String, color: Color) {
+    Text(text, style = hint2, color = color)
+}
+
+
+data class MessageState(val text: String, val color: Color)
