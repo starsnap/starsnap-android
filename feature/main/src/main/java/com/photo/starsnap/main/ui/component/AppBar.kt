b4ddf30ca553a8194bf45ed8fcdab4a4f98bbb0e
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-02-16 21:48:26 +0900

    :zap: :: fix SignupAppBar

--- feature/main/src/main/java/com/photo/starsnap/main/ui/component/AppBar.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/component/AppBar.kt
@@ -9,20 +9,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.photo.starsnap.designsystem.R
-import com.photo.starsnap.designsystem.text.CustomTextStyle.TitleMedium
+import com.photo.starsnap.designsystem.text.CustomTextStyle.TopBarTitle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignupAppBar(back: () -> Unit) {
     CenterAlignedTopAppBar(
+        expandedHeight = 56.dp,
         title = {
             Text(
-                stringResource(R.string.signup_top_bar_title), maxLines = 1, style = TitleMedium
+                text = stringResource(R.string.signup_top_bar_title),
+                maxLines = 1,
+                style = TopBarTitle,
+                textAlign = TextAlign.Center
             )
         },
         navigationIcon = {
-            IconButton(back) {
+            IconButton(onClick = back) {
                 Icon(
                     imageVector = ImageVector.vectorResource(R.drawable.back_arrow),
                     contentDescription = "Localized description"
