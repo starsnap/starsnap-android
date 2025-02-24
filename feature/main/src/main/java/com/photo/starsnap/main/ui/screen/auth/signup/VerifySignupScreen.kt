10409f4c8f9e7e84d3b6fec17be69fc63302702e
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-02-24 22:47:26 +0900

    :zap: :: add resendMail timer

--- feature/main/src/main/java/com/photo/starsnap/main/ui/screen/auth/signup/VerifySignupScreen.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/screen/auth/signup/VerifySignupScreen.kt
@@ -2,33 +2,45 @@ package com.photo.starsnap.main.ui.screen.auth.signup
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.photo.starsnap.designsystem.R
 import com.photo.starsnap.designsystem.text.CustomTextStyle.SignupTitle
+import com.photo.starsnap.designsystem.text.CustomTextStyle.hint1
+import com.photo.starsnap.main.ui.component.CheckVerifyCodeStatusMessage
 import com.photo.starsnap.main.ui.component.NextButton
 import com.photo.starsnap.main.ui.component.SignupAppBar
+import com.photo.starsnap.main.ui.component.TextButton
 import com.photo.starsnap.main.ui.component.VerifyCodeEditText
+import com.photo.starsnap.main.ui.component.VerifyCodeTimer
 import com.photo.starsnap.main.utils.NavigationRoute.SIGNUP_CONSENT_ROUTE
 import com.photo.starsnap.main.viewmodel.auth.SignupViewModel
+import com.photo.starsnap.main.viewmodel.auth.VerifyCodeState
 
 @Composable
 fun VerifySignupScreen(viewModel: SignupViewModel, navController: NavController) {
 
     val uiState by viewModel.uiState.collectAsState()
     val timerUiState by viewModel.timerUiState.collectAsState()
+
+    val resendButtonText by remember {
+        derivedStateOf {
+            if (timerUiState.resendTime == 0L) "인증번호 재전송" else "${timerUiState.resendTime}초 후 재전송 가능"
+        }
+    }
 
     LaunchedEffect(uiState.verifyCodeState) {
         // 코드 인증 성공시 약관동의 화면으로 이동
@@ -51,24 +63,26 @@ fun VerifySignupScreen(viewModel: SignupViewModel, navController: NavController)
     ) { innerPadding ->
         Column(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(horizontal = 35.dp)
                 .padding(innerPadding)
         ) {
 
-            val uiState by viewModel.uiState.collectAsState()
-            var verifyCode by remember { mutableStateOf("") }
-
             Text(
                 stringResource(R.string.signup_verify_screen_title),
                 style = SignupTitle,
-                modifier = Modifier.padding(top = 30.dp)
+                modifier = Modifier.padding(top = 30.dp, bottom = 12.dp)
             )
+
+            CheckVerifyCodeStatusMessage(viewModel)
 
             Spacer(Modifier.height(55.dp))
 
-            VerifyCodeEditText { verifyCode = it }
+            VerifyCodeEditText({ viewModel.verifyCode = it }, timerUiState.isTimerRunning)
 
-            TextButton("인증번호 재전송", { viewModel.sendEmail() }, hint1, Modifier.fillMaxWidth())
+            Spacer(Modifier.height(24.dp))
+
+            TextButton(resendButtonText , { viewModel.sendEmail() }, hint1, Modifier.fillMaxWidth(), timerUiState.resendTime == 0L)
 
             Spacer(Modifier.height(31.dp))
 
