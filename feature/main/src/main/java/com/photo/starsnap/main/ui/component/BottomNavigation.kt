0a9f3211a5ce3990628ac2547e6c8165fcd50641
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-03-16 12:54:23 +0900

    :zap: :: set BottomNavigation

---
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/component/BottomNavigation.kt
@@ -0,0 +1,66 @@
+package com.photo.starsnap.main.ui.component
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.photo.starsnap.main.utils.BottomNavItem
+
+@Composable
+fun BottomNavigation(
+    navHostController: NavHostController,
+) {
+    val bottomNavItems = remember {
+        listOf(
+            BottomNavItem.Home,
+            BottomNavItem.AddSnap,
+            BottomNavItem.User,
+            BottomNavItem.Star,
+        )
+    }
+
+    val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        bottomNavItems.forEach { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(id = item.iconRes),
+                        contentDescription = item.label,
+                    )
+                },
+                label = {
+                    Text(
+                        text = item.label
+                    )
+                },
+                alwaysShowLabel = true,
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                onClick = {
+                    navHostController.navigate(item.route) {
+                        popUpTo(navHostController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
+        }
+    }
+}
---
+++ feature/main/src/main/java/com/photo/starsnap/main/utils/BottomNavItem.kt
@@ -0,0 +1,41 @@
+package com.photo.starsnap.main.utils
+
+import com.photo.starsnap.designsystem.R
+
+sealed class BottomNavItem(
+    val label: String,
+    val iconRes: Int,
+    val route: String,
+) {
+
+    companion object {
+        private const val HOME_ROUTE = "HOME"
+        private const val USER_ROUTE = "USER"
+        private const val ADD_SNAP_ROUTE = "ADD_SNAP"
+        private const val STAR_HUB_ROUTE = "STAR_HUB"
+    }
+
+    data object Home : BottomNavItem(
+        label = "Home",
+        iconRes = R.drawable.home_icon,
+        route = HOME_ROUTE,
+    )
+
+    data object AddSnap : BottomNavItem(
+        label = "AddSnap",
+        iconRes = R.drawable.add_icon,
+        route = ADD_SNAP_ROUTE
+    )
+
+    data object User : BottomNavItem(
+        label = "User",
+        iconRes = R.drawable.user_icon,
+        route = USER_ROUTE,
+    )
+
+    data object Star : BottomNavItem(
+        label = "StarHub",
+        iconRes = R.drawable.star_icon,
+        route = STAR_HUB_ROUTE,
+    )
+}
