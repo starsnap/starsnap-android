c7ec9383434ea50da2095c814c00fb279dfaf687
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-09-07 22:23:28 +0900

    :zap: :: add logging for screen navigation in multiple screens

--- feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/profile/UserScreen.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/profile/UserScreen.kt
@@ -1,8 +1,26 @@
 package com.photo.starsnap.main.ui.screen.main.profile
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.photo.starsnap.designsystem.CustomColor
 
 @Composable
 fun UserScreen() {
+    LaunchedEffect(Unit) {
+        Log.d("화면", "UserScreen")
+    }
+    Scaffold(
+        containerColor = CustomColor.container
+    ) { padding ->
+        Column(
+            Modifier.padding(padding)
+        ) {
 
+        }
+    }
 }
