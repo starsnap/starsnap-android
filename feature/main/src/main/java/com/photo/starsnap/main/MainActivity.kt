49ef1b83379880185e1fb28bce8be291d9332503
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-09-07 23:04:25 +0900

    :zap: refactor MainActivity to remove splash screen installation and pass navController to MainRoute

--- feature/main/src/main/java/com/photo/starsnap/main/MainActivity.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.photo.starsnap.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.photo.starsnap.main.route.root_route.AuthRoute
@@ -15,16 +14,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashscreen = installSplashScreen()
+//        val splashscreen = installSplashScreen()
 
         setContent {
 //            val splashScreenState = remember { mutableStateOf(true) }
 //            splashscreen.setKeepOnScreenCondition { splashScreenState.value }
+
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = AUTH_ROUTE) {
                 AuthRoute(navController)
-                MainRoute()
+                MainRoute(navController)
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
