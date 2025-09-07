7b4a4ff1c35a538c1cc70a362dbb083d95dc9e23
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-09-07 22:36:23 +0900

    :zap: :: add logging for screen navigation in PickStarGroupScreen, PickStarScreen, and ProfileScreen

--- feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/profile/ProfileScreen.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/profile/ProfileScreen.kt
@@ -1,9 +1,27 @@
 package com.photo.starsnap.main.ui.screen.main.profile
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.photo.starsnap.designsystem.CustomColor
 
 @Composable
 fun ProfileScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        Log.d("화면", "ProfileScreen")
+    }
+    Scaffold(
+        containerColor = CustomColor.container
+    ) { padding ->
+        Column(
+            Modifier.padding(padding)
+        ) {
 
+        }
+    }
 }
--- feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/upload/PickStarGroupScreen.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/upload/PickStarGroupScreen.kt
@@ -1,10 +1,14 @@
 package com.photo.starsnap.main.ui.screen.main.upload
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavController
 import com.photo.starsnap.main.viewmodel.main.UploadViewModel
 
 @Composable
 fun PickStarGroupScreen(navController: NavController, uploadViewModel: UploadViewModel) {
-
+    LaunchedEffect(Unit) {
+        Log.d("화면", "PickStarGroupScreen")
+    }
 }
