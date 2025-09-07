7fa3f96cf4dc7f824e8284f2ba3fde2390f79d70
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-09-07 23:04:30 +0900

    :heavy_plus_sign: :: add GALLERY_PHOTO_SIZE constant to Constant.kt for consistent photo size management

--- feature/main/src/main/java/com/photo/starsnap/main/utils/constant/Constant.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/utils/constant/Constant.kt
@@ -2,6 +2,7 @@ package com.photo.starsnap.main.utils.constant
 
 object Constant {
     const val SNAP_SIZE = 50
+    const val GALLERY_PHOTO_SIZE = 50
     private const val REGION = "ap-northeast-2"
     private const val BUCKET_NAME = "starsnap"
 
