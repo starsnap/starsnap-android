ea4d7162a0819bfb6e2acc6b1d8370d8f1526077
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-09-09 23:56:18 +0900

    :zap: refactor SetSnapScreen to replace LazyHorizontalGrid with HorizontalPager for improved photo display and layout

--- feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/upload/SetSnapScreen.kt
+++ feature/main/src/main/java/com/photo/starsnap/main/ui/screen/main/upload/SetSnapScreen.kt
@@ -1,22 +1,20 @@
 package com.photo.starsnap.main.ui.screen.main.upload
 
 import android.util.Log
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,30 +31,31 @@ fun SetSnapScreen(navController: NavController, uploadViewModel: UploadViewModel
         Log.d("화면", "SetSnapScreen")
     }
     val selectedPhotos by uploadViewModel.selectedPhotos.collectAsStateWithLifecycle()
+    val pagerState = rememberPagerState(pageCount = { selectedPhotos.size })
 
     Scaffold(
         topBar = { TopAppBar("새로운 스냅", navController) },
         containerColor = CustomColor.container,
     ) { padding ->
-        LazyHorizontalGrid(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp),
-            rows = GridCells.Fixed(1)
+                .padding(padding)
+                .fillMaxSize()
         ) {
-            items(selectedPhotos.size) { index ->
-                val image = selectedPhotos[index]
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+            ) { page ->
+                val photo = selectedPhotos[page]
                 GlideImage(
                     modifier = Modifier
-                        .padding(4.dp)
-                        .fillMaxHeight()
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(8.dp))
-                        .clickable {
-                        },
-                    imageModel = { image.imageUri },
+                        .animateContentSize()
+                        .fillMaxSize(),
+                    imageModel = { photo.imageUri },
                     imageOptions = ImageOptions(
-                        contentScale = ContentScale.Crop,
+                        contentScale = ContentScale.FillHeight,
                         alignment = Alignment.Center,
                         alpha = 1f
                     )
